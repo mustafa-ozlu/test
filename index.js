703c2f625b3faaff8950a83813d844d7fadaa372
wallace09 <mustafa09@yahoo.com>
2022-11-10 17:30:17 +0300

    index.js updated

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const dugme = document.getElementById('sil')
-const div = document.getElementById('container')
-console.log(div)
+const dugme = document.getElementById("sil");
+const div = document.getElementById("container");
+console.log(div);
 
-dugme.addEventListener('click',function(){
-    console.log(dugme.innerText)
-    if (dugme.innerText == 'geri getir') {
-        div.innerHTML = `<h1 >gjgjgjhgjh</h1>
+dugme.addEventListener("click", function () {
+  console.log(dugme.innerText);
+  if (dugme.innerText == "geri getir") {
+    div.innerHTML = `<h1 >gjgjgjhgjh</h1>
         <h1 >mustafa</h1>
-        <h1 >gjgjgjhgjh</h1>`
-        
-    }else{
-        div.innerHTML = ''
-        dugme.innerText = 'geri getir'
+        <h1 >gjgjgjhgjh</h1>`;
+  } else {
+    div.innerHTML = "";
+    dugme.innerText = "geri getir";
+  }
+  console.log("buton clicked");
+});
 
-    }
-console.log('buton clicked')
-})
+    
